a00a03bedeba0c2eb5d82ba6261a8de446ca8f26
Orpheus <orpheus@mampang.com>
2023-08-14 21:31:30 +0200

    better download

--- scripts/download_last_wikipedia_dump.sh
+++ scripts/download_last_wikipedia_dump.sh
@@ -8,10 +8,22 @@ then
   exit 0
 fi
 
-# dumps
-DUMP_URL="https://dumps.wikimedia.org/${LANG}wiki/latest/${LANG}wiki-latest-pages-articles.xml.bz2"
-DESTINATION_FILENAME="${LANG}wiki-latest-pages-articles.xml.bz2"
+# Identify dump files
+DUMPURL_BASE="https://dumps.wikimedia.org/${LANG}wiki/latest/"
+DUMPFILE_LIST=$(curl -q ${DUMPURL_BASE} 2>&1 | grep "href=\"${LANG}" | grep "latest-pages-articles[0-9]" | awk -F'"' '{ print $2 }' | grep -v "\-rss" | sort)
 
-mkdir -p DATA/dump/${LANG}
-curl -o DATA/dump/${LANG}/${DESTINATION_FILENAME} -L -O -C - ${DUMP_URL}
+# Setting destination directory
+DESTINATION_DIR="DATA/dump/${LANG}"
+mkdir -p ${DESTINATION_DIR}
+
+# Download dump files if not already done
+# Don't bother doing it in parallel as the site allows only two simultaneous sessions (and we want to be nice :-))
+for DUMPFILE in ${DUMPFILE_LIST}
+do
+  DUMPURL="${DUMPURL_BASE}${DUMPFILE}"
+
+  echo "Downloading ${DUMPFILE}"
+  curl -o ${DESTINATION_DIR}/${DUMPFILE} -L -O -C - ${DUMPURL}
+
+done
 
