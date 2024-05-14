75f2e10bb72e6b7a2c129dca52969d7b3bbdd32d
Orpheus <orpheus@mampang.org>
2024-05-14 15:14:09 +0200

    graph

---
+++ scripts/09_igraph_analysis.py
@@ -0,0 +1,109 @@
+#%% Connection
+import pandas as pd
+import duckdb
+import igraph as ig
+
+con = duckdb.connect("../wiki.db")
+
+# nodes
+# category_type
+# category_node_assoc
+# portal_type
+# portal_node_assoc
+# redirections
+# links_nodes
+
+print(
+    con.sql(
+        """
+select
+*
+from
+portal_type
+limit 5
+"""
+    )
+)
+
+print(
+    con.sql(
+        """
+select
+*
+from
+portal_node_assoc
+limit 5
+"""
+    )
+)
+
+print(
+    con.sql(
+        """
+select
+*
+from
+nodes
+limit 5
+"""
+    )
+)
+
+# nodes request
+vertices = con.sql(
+    """
+select
+T3.title as name
+from
+portal_type T1 inner join
+portal_node_assoc T2 on (T1.id = T2.portal_id) inner join
+nodes T3 on (T2.node_id = T3.id)
+where
+T1.name ilike '%video game%'
+and
+T3.namespace = 0
+group by 1
+"""
+).to_df()
+
+print(vertices)
+
+# edge request
+edges = con.sql(
+    """
+with node_selection as (
+    select
+    T3.id,
+    T3.title
+    from
+    portal_type T1 inner join
+    portal_node_assoc T2 on (T1.id = T2.portal_id) inner join
+    nodes T3 on (T2.node_id = T3.id)
+    where
+    T1.name ilike '%video game%'
+    and
+    T3.namespace = 0
+    group by 1,2
+),
+edges as (
+    select
+    T2.title as source_name,
+    T3.title as destination_name
+    from
+    links_nodes T1 inner join
+    node_selection T2 on (T1.source_node_id = T2.id) inner join
+    node_selection T3 on (T1.destination_node_id = T3.id)
+    group by 1,2
+)
+select
+source_name,
+destination_name
+from
+edges
+"""
+).to_df()
+
+print(edges)
+
+# Graph
+g = ig.Graph.DataFrame(edges=edges, directed=True, vertices=vertices, use_vids=False)
--- scripts/10_query_undertale.py
+++ scripts/10_query_undertale.py
@@ -1,31 +1,37 @@
 #%% Connection
 import duckdb
 
-con = duckdb.connect("wiki.db")
+con = duckdb.connect("../wiki.db")
 
 #%% Structure tables des nodes
-print(con.sql(
-    """
+print(
+    con.sql(
+        """
 select
 *
 from
 nodes limit 5
 """
-))
+    )
+)
 
 #%% Structure de links_node
-print(con.sql(
-    """
+print(
+    con.sql(
+        """
 select
 *
 from
 links_nodes
 limit 5
 """
-))
+    )
+)
 
 #%% Comptage par popularité des lien entrants
-print(con.sql("""
+print(
+    con.sql(
+        """
 select
 *
 from
@@ -33,11 +39,14 @@ nodes T1
 where
 T1.title = 'Undertale'
 """
-))
+    )
+)
 
 #%%
 
-print(con.sql("""
+print(
+    con.sql(
+        """
 select
 T1.title,
 T3.title
@@ -48,4 +57,5 @@ nodes T3 on (T2.source_node_id = T3.id)
 where
 T1.title = 'Undertale'
 """
-))
+    )
+)
