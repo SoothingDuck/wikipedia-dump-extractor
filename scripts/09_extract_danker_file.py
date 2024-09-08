b8756253417e39168c9be28e1e031672e926932e
orpheus <orpheus@mampang.com>
2024-09-08 11:53:17 +0200

    video games or all

--- scripts/09_extract_danker_file.py
+++ scripts/09_extract_danker_file.py
@@ -7,11 +7,16 @@ pd.set_option("display.max_columns", 1000)
 pd.set_option("display.width", 1000)
 
 # %% Connection
-con = duckdb.connect("../wiki.db")
+con = duckdb.connect("wiki.db")
+
+# %% Variable
+# mode = "video games"
+mode = "all"
 
 # %% nodes request
-links = con.sql(
-    """
+if mode == "video games":
+    links = con.sql(
+        """
 with RAW_NODES as (
     select
     T3.id,
@@ -47,12 +52,54 @@ from
 group by 1,2
 order by 1,2
 """
-).to_df()
+    ).to_df()
+else:
+    links = con.sql(
+        """
+with RAW_NODES as (
+    select
+    T3.id,
+    T3.title as name
+    from
+    nodes T3
+    where
+    T3.namespace = 0
+    group by 1,2
+), RAW_EDGES as (
+    select
+    T1.source_node_id,
+    T1.destination_node_id,
+    T2.name as source_name,
+    T3.name as destination_name
+    from
+    links_nodes T1 inner join
+    RAW_NODES T2 on (T1.source_node_id = T2.id) inner join
+    RAW_NODES T3 on (T1.destination_node_id = T3.id)
+    group by 1,2,3,4
+)
+select
+    source_node_id,
+    destination_node_id
+from
+    RAW_EDGES
+group by 1,2
+order by 1,2
+"""
+    ).to_df()
+
 
 # %% Write danker_output
-links.to_csv(
-    "../danker_links_video_games.tsv",
-    sep="\t",
-    header=False,
-    index=False,
-)
+if mode == "video games":
+    links.to_csv(
+        "danker_links_video_games.tsv",
+        sep="\t",
+        header=False,
+        index=False,
+    )
+else:
+    links.to_csv(
+        "danker_links_all.tsv",
+        sep="\t",
+        header=False,
+        index=False,
+    )
